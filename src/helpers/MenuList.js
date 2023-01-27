49face6b142d38bbc775588292933122fef69fa2
Rishi Pathania <rishipathania98@gmail.com>
2023-01-27 17:57:01 +0530

    first commit

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import Pepperoni from "../assets/pepperoni.jpg";
+import Margherita from "../assets/margherita.jpg";
+import PedroTechSpecial from "../assets/pedrotechspecial.jpg";
+import Vegan from "../assets/vegan.jpg";
+import Pineapple from "../assets/pineapple.jpg";
+import Expensive from "../assets/expensive.jpg";
+
+export const MenuList = [
+  {
+    name: "Pepperoni Pizza",
+    image: Pepperoni,
+    price: 550,
+  },
+  {
+    name: "Margherita Pizza",
+    image: Margherita,
+    price: 400,
+  },
+  {
+    name: "Delicious Special Pizza",
+    image: PedroTechSpecial,
+    price: 1199,
+  },
+  {
+    name: "Vegan Pizza",
+    image: Vegan,
+    price: 999,
+  },
+  {
+    name: "Pineapple Pizza",
+    image: Pineapple,
+    price: 399,
+  },
+  {
+    name: "Very Expensive Pizza",
+    image: Expensive,
+    price: 2999,
+  },
+];
